f9d412a733628c183270cf963074678a44573682
KhaledAlabssi <khaled.absi5@gmail.com>
2024-11-04 21:48:49 +0100

    adding lect 1 and 2 files to the repo

---
+++ lect1.js
@@ -0,0 +1,56 @@
+// syntax: is the way we write code in xx langugage
+// statment
+// this is a comment
+//  to describe code
+//  to structue
+//  to share info/tips/warnings with other developer
+//  reminder/to-dos
+
+// variables:
+// const, let, var: = keyword, which reserved by js
+
+// variable syntax:
+//  keyword varName = value
+
+let numberb;
+// Initiating variable
+let numberA = 5;
+// Giving a value to a variable called: assigning value to variable
+
+// let: can be changed and reassigned
+// const: can NOT be changed or reassigned
+let userName = "Sarah"
+userName = "Sam"
+userName = false
+// var = value, and type
+console.log(userName);
+
+
+// always remember to use CONST
+
+
+const userN = 9
+userN = 7
+// var = value, and type
+console.log(userN)
+
+// Data types:
+// number: 1, 0.3, -351  
+// boolean: true, false
+// string: "fdask 4232 # $"
+// string: 'fdsa % 55 <'
+// string: `fdsa % 55 <`
+// objects:  array []
+//          object {}
+// date:
+
+
+
+
+
+
+
+
+
+
+
